4e37db79dd22cb05266c4b931c12dc2eeb8f4ffe
Hunter3ds <ghostmat817@gmail.com>
2024-10-18 16:42:53 -0300

    adição de:style.css e modificação de index.html e script.js

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ const apiKey = 'eeb2955a3047c339352d64892f0a4122'
 
 async function getWeatherByCity(city){
     try{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt`) 
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt`)
+
         const data = await response.json()
 
-        console.log(`Clima em ${city}: ${data.weather[0].description} ${data.main.temp}`)
+        if(response.ok){
+            document.querySelector('#weatherResult').innerHTML = `<p>O clima na cidade de ${city} é:  ${data.main.temp} com ${data.weather[0].description} </p>`
+        }
     }
     catch(error){
         console.log(error)
-    }
+    }   
 }
 
-getWeatherByCity("Belém")
+document.getElementById('getWeather').addEventListener('click', ()=>{
+    const city = document.getElementById('city').value;
+
+    if(city){
+        getWeatherByCity(city);
+    }else{
+        document.getElementById('WeatherResult').innerHTML = `<p>Por favor insira um nome valido</p>`
+        alert('não deu certo');
+    }
+})
